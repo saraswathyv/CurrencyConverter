93ca7f4f8321245d6b9b42b49ba63ca62b33c877
Saraswathy <saraswathy.techspot@gmail.com>
2021-03-08 18:27:41 +0530

    updated docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
     path: 'about',
     loadChildren: () => import('../app/pages/about/about.module').then(mod => mod.AboutModule)
   },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
